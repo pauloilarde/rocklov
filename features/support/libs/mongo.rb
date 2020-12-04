dfec5888bebfe8f8e528e56a948982ba096024da
Paulo Ilarde <pauloilarde@gmail.com>
2020-12-04 19:06:41 -0300

    Finalizando o módulo que utiliza a versão do api RockLov

--- features/support/libs/mongo.rb
+++ features/support/libs/mongo.rb
@@ -1,9 +1,28 @@
+require "mongo"
+
 Mongo::Logger.logger = Logger.new("./logs/mongo.log")
 
 class MongoDB
-  def remove_user(email)
+  attr_accessor :users, :equipos
+
+  def initialize
     client = Mongo::Client.new("mongodb://rocklov-web:27017/rocklov")
-    users = client[:users]
-    users.delete_many({ email: email })
+    @users = client[:users]
+    @equipos = client[:equipos]
+  end
+
+  def remove_user(email)
+    @users.delete_many({ email: email })
+  end
+
+  def get_user(email)
+    user = @users.find({ email: email }).first
+    return user[:_id]
+  end
+
+  def remove_equipo(name, email)
+    user_id = get_user(email)
+
+    @equipos.delete_many({ name: name, user: user_id })
   end
 end
