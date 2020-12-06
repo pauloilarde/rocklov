095863d2bca8ddf233bf5e809b11fcf05b9ac399
Paulo Ilarde <pauloilarde@gmail.com>
2020-12-06 18:58:51 -0300

    Implementando código para escolher o browser

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,8 +6,21 @@ require "mongo"
 
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["CONFIG"]}"))
 
+BROWSER = ENV["BROWSER"]
+
+if BROWSER == "firefox"
+  @driver = :selenium
+elsif BROWSER == "fire_headless"
+    @driver = :selenium_headless
+elsif BROWSER == "chrome"
+  @driver = :selenium_chrome
+else
+  @driver = :selenium_chrome_headless
+
+  
+
 Capybara.configure do |config|
-  config.default_driver = :selenium
+  config.default_driver = @driver
   config.app_host = CONFIG["url"]
   config.default_max_wait_time = 10
 end
